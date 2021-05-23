d77ad350c8ea54510a6f9c858e1a72da5154ccbe
ohtjqkd <ohtjqkd@gmail.com>
2021-05-23 22:08:59 +0900

    flask server dash app

--- dashapp.py
+++ dashapp.py
@@ -1,3 +1,5 @@
+from flask import Flask
+from dash import Dash
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -5,8 +7,8 @@ import plotly.express as px
 import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = Flask(__name__)
+app = Dash(__name__, external_stylesheets=external_stylesheets, server = server)
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
