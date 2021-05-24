53d3bf9dd49dd9c039a7841dc8116939ef92d31d
ohtjqkd <ohtjqkd@gmail.com>
2021-05-24 19:49:41 +0900

    static html

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request
+from dash import Dash
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+server = Flask(__name__)
+# app = dash.Dash(__name__, external_stylesheets=external_stylesheets, server = server)
+
+@server.route("/")
+def get_eda():
+    return render_template('eda.html')
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+# df = pd.DataFrame({
+#     "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+#     "Amount": [4, 1, 2, 2, 4, 5],
+#     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+# })
+
+# fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+# app.layout = html.Div(children=[
+#     html.H1(children='Hello Dash'),
+
+#     html.Div(children='''
+#         Dash: A web application framework for Python.
+#     '''),
+
+#     dcc.Graph(
+#         id='example-graph',
+#         figure=fig
+#     )
+# ])
+
+# print(html.Div(children=[
+#     html.H1(children='Hello Dash'),
+
+#     html.Div(children='''
+#         Dash: A web application framework for Python.
+#     '''),
+
+#     dcc.Graph(
+#         id='example-graph',
+#         figure=fig
+#     )
+# ]))
+
+if __name__ == '__main__':
+    server.run(host='0.0.0.0', port=8000, debug=True)
+    # app.run_server(debug=True)
